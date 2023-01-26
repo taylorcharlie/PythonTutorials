00c92a21216719453ef8b7ab396a5cf20f4e3626
taylorcharlie <122097408+taylorcharlie@users.noreply.github.com>
2023-01-26 15:58:20 +0000

    currentTime.py
    This is basic python code to present to you the current time on that day when run.

--- currentTime.py
+++ currentTime.py
@@ -1,1 +1,13 @@
 
+from datetime import datetime
+  
+# The now() mehtod is used
+# to get the current time
+# on the date that is now.
+now_method = datetime.now()
+  
+# strftime() method used to
+# get the hour,minute and second
+# current time.
+currentTime = now_method.strftime("%H:%M:%S")
+print("Current Time =", currentTime)
